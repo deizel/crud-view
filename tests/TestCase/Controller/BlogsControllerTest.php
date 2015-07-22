bb06e614cf41e6d23eece1758686a5ba9e4883e7
Chris Burke <chris.burke@cakedc.com>
2015-07-22 01:37:45 -0500

    Add assertions for view action

--- tests/TestCase/Controller/BlogsControllerTest.php
+++ tests/TestCase/Controller/BlogsControllerTest.php
@@ -11,21 +11,33 @@ class BlogsControllerTest extends IntegrationTestCase {
     {
         $this->get('/blogs/index');
         $this->assertResponseSuccess();
+        $this->assertResponseContains('<title>Blogs</title>');
         $this->assertCommon();
 
         $this->assertResponseContains('<div class="scaffold-action scaffold-action-index scaffold-controller-blogs scaffold-blogs-index">');
-        $this->assertResponseContains('Page 1 of 2, showing 3 records out of 5 total.');
         $this->assertResponseContains('<h2>Blogs</h2>');
-        $this->assertResponseContains('<a href="/blogs/add" class="btn btn-default">Add</a>');
-        $this->assertResponseContains('<a href="/blogs/lookup" class="btn btn-default">Lookup</a>');
         $this->assertResponseContains('<a href="/blogs/view/1" class="btn btn-default">View</a>');
         $this->assertResponseContains('<a href="/blogs/edit/1" class="btn btn-default">Edit</a>');
         $this->assertResponseContains('<a href="#" onclick="if (confirm(&quot;Are you sure you want to delete record #1?&quot;');
+        $this->assertResponseContains('Page 1 of 2, showing 3 records out of 5 total.');
+    }
+
+    public function testView()
+    {
+        $this->get('/blogs/view/1');
+        $this->assertResponseSuccess();
+        $this->assertResponseContains('<title>View Blog #1: 1st post</title>');
+        $this->assertCommon();
+
+        $this->assertResponseContains('<div class="scaffold-action scaffold-action-view scaffold-controller-blogs scaffold-blogs-view">');
+        $this->assertResponseContains('<h2>View Blog #1: 1st post</h2>');
+        $this->assertResponseContains('<tr><th>Is Active</th><td><span class="label label-success">Yes</span>&nbsp;</td></tr>');
+        $this->assertResponseContains('<tr><th>Name</th><td><a href="/blogs/view/1">1st post</a>&nbsp;</td></tr>');
+        $this->assertResponseContains('<tr><th>Body</th><td>1st post body&nbsp;</td></tr>');
     }
 
     public function assertCommon()
     {
-        $this->assertResponseContains('<title>Blogs</title>');
         $this->assertResponseContains('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.css"/>');
         $this->assertResponseContains('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.14.30/css/bootstrap-datetimepicker.min.css"/>');
         $this->assertResponseContains('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.1/css/selectize.bootstrap3.min.css"/>');
@@ -38,5 +50,7 @@ class BlogsControllerTest extends IntegrationTestCase {
         $this->assertResponseContains('<script src="https://cdn.jsdelivr.net/jquery.dirtyforms/1.2.2/jquery.dirtyforms.min.js"></script>');
         $this->assertResponseContains('<a class="navbar-brand" href="/">Crud View</a>');
         $this->assertResponseContains('<li><a href="/blogs">Blogs</a></li>');
+        $this->assertResponseContains('<a href="/blogs/add" class="btn btn-default">Add</a>');
+        $this->assertResponseContains('<a href="/blogs/lookup" class="btn btn-default">Lookup</a>'); // @todo This shouldn't appear.
     }
 }
